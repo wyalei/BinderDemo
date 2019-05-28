a01ee5b2a44c086ce9d262f74537597156dc4c0a
wangyalei <wangyalei912@gmail.com>
2019-05-28 15:21:57 +0800

    add version code

--- app/src/main/java/com/richtech/binderdemo/MainActivity.java
+++ app/src/main/java/com/richtech/binderdemo/MainActivity.java
@@ -1,13 +1,17 @@
 package com.richtech.binderdemo;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.TextView;
 
 import com.binder.BinderManager;
 import com.binder.server.IServerBinder;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mInfoTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +26,20 @@ public class MainActivity extends AppCompatActivity {
 
         IServerBinder serverBinder = BinderManager.getInstance().bind(IServerBinder.class);
         Log.d("binder2", "simpleName: " + serverBinder.add(100, 2));
+
+        mInfoTv = findViewById(R.id.info_tv);
+        mInfoTv.setText(String.valueOf(getVersionCode()));
+    }
+
+    private int getVersionCode(){
+        int code = 1;
+        try {
+            code = getPackageManager().getPackageInfo(getPackageName(), 0).versionCode;
+        }catch (PackageManager.NameNotFoundException ex){
+            ex.printStackTrace();
+        }
+
+        return code;
     }
 
     @Override
